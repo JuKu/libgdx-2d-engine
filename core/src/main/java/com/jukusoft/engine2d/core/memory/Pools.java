eea3d36abb6b18abdf1b53dfd1f30c12d5aa6c68
juku <kuenzel.justin@t-online.de>
2019-08-04 19:26:01 +0200

    Add memory pools

---
+++ core/src/main/java/com/jukusoft/engine2d/core/memory/Pools.java
@@ -0,0 +1,46 @@
+package com.jukusoft.engine2d.core.memory;
+
+import com.jukusoft.engine2d.core.events.EventData;
+import com.jukusoft.engine2d.core.logger.Log;
+import org.mini2Dx.gdx.utils.Pool;
+
+/**
+ * Object pools to reuse memory to avoid memory allocation
+ *
+ * @see Pool
+ */
+public class Pools {
+
+    protected Pools() {
+        //
+    }
+
+    public static <T> T get (Class<T> cls) {
+        T obj = org.mini2Dx.gdx.utils.Pools.get(cls).obtain();
+
+        if (obj == null) {
+            try {
+                obj = cls.newInstance();
+            } catch (InstantiationException | IllegalAccessException e) {
+                Log.w("Memory", "Exception while instantiating a new Pool object: ", e);
+            }
+        }
+
+        if (obj instanceof EventData) {
+            //initialize event
+            ((EventData) obj).init();
+        }
+
+        return obj;
+    }
+
+    public static <T> void free (T obj) {
+        if (obj instanceof Pool.Poolable) {
+            ((Pool.Poolable) obj).reset();
+        }
+
+        Pool<T> pool = (Pool<T>) org.mini2Dx.gdx.utils.Pools.get(obj.getClass());
+        pool.free(obj);
+    }
+
+}
