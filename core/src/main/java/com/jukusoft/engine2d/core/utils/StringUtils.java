29244799ce0d6aad1f2c8618ab8861f55687ea67
juku <kuenzel.justin@t-online.de>
2019-08-20 01:43:14 +0200

    Add StringUtils to check for null and empty strings

---
+++ core/src/main/java/com/jukusoft/engine2d/core/utils/StringUtils.java
@@ -0,0 +1,21 @@
+package com.jukusoft.engine2d.core.utils;
+
+import java.util.Objects;
+
+public class StringUtils {
+
+    private StringUtils () {
+        //
+    }
+
+    public static void checkNotNullAndNotEmpty (String str) {
+        checkNotNullAndNotEmpty(str, "string");
+    }
+
+    public static void checkNotNullAndNotEmpty (String str, String messageObj) {
+        Objects.requireNonNull(str);
+
+        if (messageObj.isEmpty()) throw new IllegalArgumentException(messageObj + " cannot be null");
+    }
+
+}
