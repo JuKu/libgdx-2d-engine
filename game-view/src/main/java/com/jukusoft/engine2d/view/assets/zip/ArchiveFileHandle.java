38abfa1b370e400b6f1e0725ac7af1bdbe5d4ffb
juku <kuenzel.justin@t-online.de>
2019-10-10 23:58:43 +0200

    Fix asset loading from zip archives

--- game-view/src/main/java/com/jukusoft/engine2d/view/assets/zip/ArchiveFileHandle.java
+++ game-view/src/main/java/com/jukusoft/engine2d/view/assets/zip/ArchiveFileHandle.java
@@ -21,13 +21,13 @@ public class ArchiveFileHandle extends FileHandle {
     final ZipEntry archiveEntry;
 
     public ArchiveFileHandle (ZipFile archive, File file) {
-        super(file, Files.FileType.External);//Classpath
+        super(file, Files.FileType.Classpath);//Classpath, External
         this.archive = archive;
         archiveEntry = this.archive.getEntry(file.getPath());
     }
 
     public ArchiveFileHandle (ZipFile archive, String fileName) {
-        super(fileName.replace('\\', '/'), Files.FileType.External);//Classpath
+        super(fileName.replace('\\', '/'), Files.FileType.Classpath);//Classpath, External
         this.archive = archive;
         this.archiveEntry = archive.getEntry(fileName.replace('\\', '/'));
     }
