b01f947decd2ee8cf2c22e3d51f8f75c086733bb
JuKu <kuenzel.justin@t-online.de>
2019-10-31 15:24:30 +0100

    Add some junit tests for ZipAssetManagerFactory

---
+++ game-view/src/test/java/com/jukusoft/engine2d/view/assets/ZipAssetManagerFactoryTest.java
@@ -0,0 +1,31 @@
+package com.jukusoft.engine2d.view.assets;
+
+import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.NoSuchFileException;
+import java.util.zip.ZipFile;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class ZipAssetManagerFactoryTest {
+
+    @Test (expected = NullPointerException.class)
+    public void testCreateNull() {
+        ZipAssetManagerFactory.create(null);
+    }
+
+    @Test (expected = NoSuchFileException.class)
+    public void testCreateNotExistingZipFile() throws IOException {
+        ZipAssetManagerFactory.create(new ZipFile("not-existing-file.zip"));
+    }
+
+    @Test
+    public void testCreateValidZip() throws IOException {
+        assertTrue(new File("../data/mods/mod1.zip").exists());
+        ZipAssetManagerFactory.create(new ZipFile("../data/mods/mod1.zip"));
+    }
+
+}
