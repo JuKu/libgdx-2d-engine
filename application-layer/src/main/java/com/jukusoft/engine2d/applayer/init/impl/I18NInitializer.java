6840ef3c3b93bd7f987f6698eff6a343c1e1f037
JuKu <kuenzel.justin@t-online.de>
2019-11-15 00:58:39 +0100

    Set logger for i18n gettext library

--- application-layer/src/main/java/com/jukusoft/engine2d/applayer/init/impl/I18NInitializer.java
+++ application-layer/src/main/java/com/jukusoft/engine2d/applayer/init/impl/I18NInitializer.java
@@ -7,15 +7,19 @@ import com.jukusoft.engine2d.core.logger.Log;
 import com.jukusoft.engine2d.core.utils.FilePath;
 import com.jukusoft.engine2d.core.utils.Utils;
 import com.jukusoft.i18n.I;
+import com.jukusoft.i18n.logger.LogUtils;
 
 import java.io.File;
 import java.util.Locale;
+import java.util.function.BiConsumer;
+import java.util.logging.Level;
 
 @InitPriority(90)
 public class I18NInitializer implements Initializer {
 
     private static final String SECTION_NAME = "I18N";
     private static final String LOG_TAG = I18NInitializer.class.getSimpleName();
+    private static final String I18N_RUNTIME_LOG_TAG = "i18n";
 
     @Override
     public void init() throws Exception {
@@ -30,6 +34,21 @@ public class I18NInitializer implements Initializer {
             i18nFolder.mkdirs();
         }
 
+        LogUtils.setLogger((level, msg) -> {
+            switch(level.getName().toUpperCase()) {
+                case "SEVERE":
+                    Log.e(I18N_RUNTIME_LOG_TAG, msg);
+                    break;
+                case "WARNING":
+                    Log.w(I18N_RUNTIME_LOG_TAG, msg);
+                case "INFO":
+                    Log.i(I18N_RUNTIME_LOG_TAG, msg);
+                    break;
+                default:
+                    Log.d(I18N_RUNTIME_LOG_TAG, msg);
+                    break;
+            }
+        });
         I.init(i18nFolder, Locale.ENGLISH, "commons");
 
         String langToken = Config.get(SECTION_NAME, "token");
