50db41bc5706af760df1d5a344007cd07d5842e1
JuKu <kuenzel.justin@t-online.de>
2019-11-30 15:49:22 +0100

    Add some junit tests

--- base-game/src/test/java/com/jukusoft/engine2d/basegame/mods/ModManagerTest.java
+++ base-game/src/test/java/com/jukusoft/engine2d/basegame/mods/ModManagerTest.java
@@ -1,6 +1,9 @@
 package com.jukusoft.engine2d.basegame.mods;
 
+import com.jukusoft.engine2d.basegame.mods.credits.CreditEntry;
 import org.junit.Test;
+
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
@@ -11,6 +14,13 @@ public class ModManagerTest {
     public void testGetInstance() {
         ModManager modManager = ModManager.getInstance();
         assertNotNull(modManager);
+    }
+
+    @Test
+    public void testListCredits() {
+        List<CreditEntry> list = ModManager.getInstance().listCredits();
+
+        //TODO: add code here
     }
 
 }
