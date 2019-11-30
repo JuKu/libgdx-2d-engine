58d94cea7592fdbe1a84b54c7e31d01580947cf3
JuKu <kuenzel.justin@t-online.de>
2019-11-30 15:24:48 +0100

    Add method to get string from inputstream

--- core/src/main/java/com/jukusoft/engine2d/core/utils/StringUtils.java
+++ core/src/main/java/com/jukusoft/engine2d/core/utils/StringUtils.java
@@ -1,6 +1,13 @@
 package com.jukusoft.engine2d.core.utils;
 
+import com.jukusoft.engine2d.core.logger.Log;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 public class StringUtils {
 
@@ -16,6 +23,17 @@ public class StringUtils {
         Objects.requireNonNull(str);
 
         if (str.isEmpty()) throw new IllegalArgumentException(messageObj + " cannot be null");
+    }
+
+    public static String getStringFromInputStream(InputStream is) {
+        Objects.requireNonNull(is);
+
+        try (InputStreamReader isr = new InputStreamReader(is); BufferedReader reader = new BufferedReader(isr)) {
+            return reader.lines().collect(Collectors.joining(System.lineSeparator()));
+        } catch (IOException e) {
+            Log.w(StringUtils.class.getSimpleName(), "IOException while reading string from inputstream: ", e);
+            return "";
+        }
     }
 
 }
--- core/src/test/java/com/jukusoft/engine2d/core/utils/StringUtilsTest.java
+++ core/src/test/java/com/jukusoft/engine2d/core/utils/StringUtilsTest.java
@@ -2,6 +2,13 @@ package com.jukusoft.engine2d.core.utils;
 
 import org.junit.Test;
 
+import java.io.BufferedInputStream;
+import java.io.ByteArrayInputStream;
+import java.io.DataInputStream;
+import java.io.InputStream;
+
+import static org.junit.Assert.assertEquals;
+
 public class StringUtilsTest {
 
     @Test(expected = NullPointerException.class)
@@ -32,6 +39,19 @@ public class StringUtilsTest {
     @Test
     public void testCheckNotNullAndNotEmptyValidString() {
         StringUtils.checkNotNullAndNotEmpty("valid string", "message object");
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testGetStringFromNullInputStream() {
+        StringUtils.getStringFromInputStream(null);
+    }
+
+    @Test
+    public void testGetStringFromInputStream() {
+        InputStream is = new ByteArrayInputStream("test".getBytes());
+        String content = StringUtils.getStringFromInputStream(is);
+
+        assertEquals("test", content);
     }
 
 }
