1e91a2edd9cb9462a5b71fe1e66030000e18561b
JuKu <kuenzel.justin@t-online.de>
2020-01-03 17:08:30 +0100

    Add basics of UIStyle

--- game-ui/src/main/java/com/jukusoft/engine2d/ui/style/UIStyle.java
+++ game-ui/src/main/java/com/jukusoft/engine2d/ui/style/UIStyle.java
@@ -1,4 +1,37 @@
 package com.jukusoft.engine2d.ui.style;
 
+import com.badlogic.gdx.files.FileHandle;
+import com.jukusoft.engine2d.core.utils.StringUtils;
+import com.jukusoft.engine2d.ui.Widget;
+import org.json.JSONObject;
+
+import java.util.Objects;
+
 public class UIStyle {
+
+    private JSONObject json;
+
+    public UIStyle(FileHandle handle) {
+        this(handle.readString("UTF-8"));
+    }
+
+    public UIStyle(String content) {
+        this(new JSONObject(content));
+    }
+
+    public UIStyle(JSONObject json) {
+        Objects.requireNonNull(json);
+        this.json = json;
+    }
+
+    public JSONObject getWidgetStyle(Class<? extends Widget> cls) {
+        String key = cls.getCanonicalName();
+
+        if (json.has(key)) {
+            return json.getJSONObject(key);
+        }
+
+        return null;
+    }
+
 }
